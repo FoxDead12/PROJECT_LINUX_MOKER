456a1b23d671722715b73f6765d4bfc3da71bad7
David Xavier <davidxavier7512@gmail.com>
2026-05-27 11:02:32 +0100

    add user cases tests

---
+++ kernel/moker/tasks/task.c
@@ -0,0 +1,100 @@
+#include "defs.h"
+
+void do_work(unsigned long long exec)
+{
+	unsigned long long i,ten_ns;
+	ten_ns=exec/10;
+	for(i=0; i<ten_ns; i++){
+		asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::);
+		asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::);
+		asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::);
+		asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::);
+		asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::);
+		asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::);
+		asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::);
+		asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::);
+//32
+
+		asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::);
+		asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::);
+		asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::);
+		asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::);
+		asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::);
+		asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::);
+		asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::);
+		asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::);
+
+//64
+		asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::);
+		asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::);
+		asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::);
+		asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::);
+		asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::);
+		asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::);
+		asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::);
+		asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::); asm volatile  ("nop" ::);
+
+	}
+}
+int main(int argc, char** argv)
+{
+	struct sched_param param;
+	unsigned long long C, T, O, time0, release;
+	unsigned int task_id,njobs,i=0;
+
+for (int i = 0; argv[i] != NULL; i++) {
+    printf("argv[%d] = %s\n", i, argv[i]);
+}
+
+	struct timespec r;
+	task_id=atoi(argv[1]);
+	C=(unsigned long long)atoll(argv[2]);
+	T=(unsigned long long)atoll(argv[3]);
+	O=(unsigned long long)atoll(argv[4])+OFFSET;
+	time0 = (unsigned long long)atoll(argv[5]);
+	njobs=atoi(argv[6]);
+
+
+	printf("Task(%d,%d): before SCHED_RM\n",task_id,getpid());
+	param.sched_priority = 0;
+	if((sched_setscheduler(0,SCHED_RM,&param)) == -1){
+		perror("ERROR:sched_setscheduler failed");
+		exit(-1);
+	}
+
+	// ... set period of task to SCHED_RM ...
+	if (syscall(SYS_MOKER_RM_SET_PERIOD, T) < 0) {
+		perror("ERROR: set moker SCHED_RM period failed");
+		exit(-1);
+	}
+
+	printf("Task(%d,%d): after SCHED_RM\n",task_id,getpid());
+
+	release = time0 + O;
+	for(i=0;i<njobs;i++){
+		r.tv_sec = release / NSEC_PER_SEC;
+		r.tv_nsec = release % NSEC_PER_SEC;
+
+
+		printf("Task(%d,%d,%d): sleeping until %lld\n",task_id,getpid(),i,release);
+		clock_nanosleep(CLOCK_MONOTONIC,TIMER_ABSTIME, &r,NULL);
+		printf("Task(%d,%d,%d): ready for execution\n",task_id,getpid(),i);
+
+		do_work(C);
+
+		//computes the next release
+		release += T;
+
+	}
+	exit(task_id);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
